a125ac860d4df1b3663bdd6c68d535a2d9c1620e
Lars Kellogg-Stedman <lars@redhat.com>
2013-11-24 18:41:01 -0500

    make app.py importable

--- app.py
+++ app.py
@@ -11,7 +11,9 @@ if 'OPENSHIFT_PYTHON_DIR' in os.environ:
         pass
 
 from pubsub import app
-app.run(server='gevent',
-        host=os.environ.get('OPENSHIFT_PYTHON_IP', '127.0.0.1'),
-        port=os.environ.get('OPENSHIFT_PYTHON_PORT', 8080))
+
+if __name__ == '__main__':
+    app.run(server='gevent',
+            host=os.environ.get('OPENSHIFT_PYTHON_IP', '127.0.0.1'),
+            port=os.environ.get('OPENSHIFT_PYTHON_PORT', 8080))
 
