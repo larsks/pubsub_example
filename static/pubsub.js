c1bea06196cd8232103e82feae1fa89bfc16dd6b
Lars Kellogg-Stedman <lars@redhat.com>
2013-11-24 10:21:21 -0500

    added license

--- static/pubsub.js
+++ static/pubsub.js
@@ -1,3 +1,23 @@
+/**********************************************************************
+ * Pub/Sub Example
+ * Copyright (C) 2013 Lars Kellogg-Stedman <lars@oddbit.com>
+ * http://github.com/larsks/pubsub_example
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 /* We need to use port 8000 when deployed on OpenShift to avoid
  * problems with their front-end proxy architecture.  You'll need
  * to set using_openshift somewhere before loading this script.
