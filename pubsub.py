c19931f7a9bc8f344e7e97af3b96e98514a18ca6
Lars Kellogg-Stedman <lars@redhat.com>
2013-11-24 23:25:01 -0500

    changes from writing article
    writing the article about this code highlited some things that were
    problematic and unnecessary.  have removed them and replaced them with
    new things that are problemetic and unnecessary :)

--- pubsub.py
+++ pubsub.py
@@ -6,6 +6,7 @@ import os
 import sys
 import argparse
 import json
+from contextlib import contextmanager
 
 import jinja2
 from jinja2.loaders import FileSystemLoader
@@ -17,6 +18,9 @@ import bottle
 # You *must* import the green version of zmq or things will
 # block and nothing we will work and you will be sad.
 from zmq import green as zmq
+
+# This lets us track how many clients are currently connected.
+polling = 0
 
 static_dir = os.path.join(
         os.environ.get('OPENSHIFT_REPO_DIR', '.'),
@@ -52,51 +56,65 @@ def pub():
         })
     return {'status': 'sent'}
 
-def worker(rfile):
-    '''A worker is spawned for each long-poll client.  The worker blocks
-    while waiting for a message to be received via subsock (a ZMQ sub
-    socket) or for a client disconnect.'''
+@contextmanager
+def subcontext():
+    '''This is a context manager that returns a ZMQ socket connected to the
+    internal message bus.  It ensures that the socket is closed when it
+    goes out of scope.'''
 
     subsock = ctx.socket(zmq.SUB)
     subsock.setsockopt(zmq.SUBSCRIBE, '')
     subsock.connect('inproc://pub')
 
-    # This is like select.poll but understands ZMQ sockets as well as
-    # any file-like object with a fileno() method.
-    poll = zmq.Poller()
-    poll.register(subsock, zmq.POLLIN)
-    poll.register(rfile, zmq.POLLIN)
+    yield subsock
 
-    while True:
+    subsock.close()
+
+def wait_for_message (rfile):
+    '''Wait for a message on the message bus and return it to the
+    client.'''
+
+    with subcontext() as subsock:
+        # This is like select.poll but understands ZMQ sockets as well as
+        # any file-like object with a fileno() method.
+        poll = zmq.Poller()
+        poll.register(subsock, zmq.POLLIN)
+        poll.register(rfile, zmq.POLLIN)
+
         events = dict(poll.poll())
 
         # A POLLIN event on rfile indicates that the client
         # has disconnected.
         if rfile.fileno() in events:
-            break
+            return
 
-        if subsock in events:
-            msg = subsock.recv_json()
-            yield(json.dumps(msg))
-            break
-
-    subsock.close()
+        # We only get this far is there's a message available on the bus.
+        msg = subsock.recv_json()
+        return msg
 
 @app.route('/sub')
 def sub():
-    '''This is the endpoint for long poll clients.  It spawns a
-    greenlet to service the request and immediately exits.'''
+    '''This is the endpoint for long poll clients.'''
 
-    # Make sure response will have the correct content type.
-    bottle.response.content_type = 'application/json'
+    global polling
 
-    # Allow cross-domain AJAX requests if we're on OpenSfhit
-    # (because polls will come in on an alternate port).
-    if using_openshift:
-        bottle.response.headers['Access-Control-Allow-Origin'] = '*'
+    try:
+        polling += 1
 
-    rfile = bottle.request.environ['wsgi.input'].rfile
-    return worker(rfile)
+        # Make sure response will have the correct content type.
+        bottle.response.content_type = 'application/json'
+
+        # Allow cross-domain AJAX requests if we're on OpenSfhit
+        # (because polls will come in on an alternate port).
+        if using_openshift:
+            bottle.response.headers['Access-Control-Allow-Origin'] = '*'
+
+        # Because "rfile" is easier to write.
+        rfile = bottle.request.environ['wsgi.input'].rfile
+
+        return wait_for_message(rfile)
+    finally:
+        polling -= 1
 
 @app.route('/<path:path>')
 def default(path):
@@ -108,4 +126,13 @@ def index():
     '''Return a default document if no path was specified.'''
     t = env.get_template('index.html')
     return t.render(using_openshift=int(using_openshift))
+
+@app.route('/debug')
+def debug():
+    '''This lets us see how many /sub requests are active.'''
+    bottle.response.content_type = 'text/plain'
+
+    # Using yield because this makes it easier to add
+    # additional output.
+    yield('polling = %d\n' % polling)
 
